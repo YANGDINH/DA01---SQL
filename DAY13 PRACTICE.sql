7ca2bf658913257dd7598baf09ff50903a76215b
YANGDINH <giangdinh016@gmail.com>
2024-06-24 11:06:42 +0700

    DAY13 PRACTICE.sql

--- DAY13 PRACTICE.sql
+++ DAY13 PRACTICE.sql
@@ -36,4 +36,62 @@ COUNT(DISTINCT b.user_id) AS monthly_active_users
 FROM July_Actions AS b
 JOIN June_Actions AS a ON b.user_id = a.user_id;
 --EX6
-
+WITH tr_by_month_country
+AS (
+SELECT 
+DATE_FORMAT(trans_date, '%Y-%M') AS month,
+country,
+COUNT(id) AS trans_count,
+SUM(amount) AS trans_total_amount
+FROM Transactions
+GROUP BY DATE_FORMAT(trans_date, '%Y-%M'), country),
+aprproved_tr_by_month_country
+AS (
+SELECT 
+DATE_FORMAT(trans_date, '%Y-%M') AS month,
+country,
+COUNT(id) AS approved_count,
+SUM(amount) AS approved_total_amount
+WHERE state='approved'
+GROUP BY DATE_FORMAT(trans_date, '%Y-%M'), country)
+SELECT a.country, a.month, a.trans_count, a.trans_total_amount, b.approved_count, b.approved_total_amount
+FROM tr_by_month_country AS a
+LEFT JOIN aprproved_tr_by_month_country AS b
+ON a.country=b.country AND a.month=b.month
+ORDER BY a.month, a.country
+--EX7
+SELECT 
+a.product_id, 
+a.year AS first_year, 
+a.quantity, 
+a.price
+FROM Sales AS a
+JOIN 
+(SELECT product_id, MIN(year) AS first_year
+FROM Sales
+GROUP BY product_id) AS b
+ON a.product_id = b.product_id AND a.year = b.first_year;
+--EX8
+WITH product_count 
+AS (
+SELECT 
+COUNT(*) AS total_products
+FROM Product),
+customer_product_count AS (
+SELECT customer_id, 
+COUNT(DISTINCT product_key) AS product_count
+FROM Customer
+GROUP BY customer_id)
+SELECT a.customer_id, b.product_count
+FROM customer_product_count AS a
+JOIN product_count AS b
+ON b.product_count = a.total_products;
+--EX9
+SELECT a.employee_id
+FROM Employees AS a
+LEFT JOIN Employees AS b ON a.manager_id = b.employee_id
+WHERE a.salary < 30000 
+AND a.manager_id IS NOT NULL 
+AND b.employee_id IS NULL
+ORDER BY a.employee_id;
+--EX10
